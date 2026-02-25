1572d18323b24e0ddaa69b993e7d3e49f3fb2691
Carlos1026-web <Derek.carlos04@gmail.com>
2026-02-24 22:34:54 -0700

    setting up

---
+++ config/Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database{
+    private $host = "localhost";
+    private $db_name = "a1";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function connect(){
+        $this -> conn = new mysqli(
+                $this->host, 
+                $this->username, 
+                $this->password, 
+                $this->db_name);
+
+        return $this->conn;        
+    }
+}
+?>
